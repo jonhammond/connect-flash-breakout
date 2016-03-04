806aea13f85b80ad38d1df9ecaf8b6c14ee4a11c
Jon Hammond <jonh1016@gmail.com>
2016-03-04 14:54:20 -0700

    Added to post route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -16,16 +16,20 @@ router.get('/', function(req, res, next) {
 
 router.post('/', validateEmail, function(req, res, next) {
   // console.log('boobs',req.body.email)
-  res.render('index');
+  res.render('index', {
+    messages: req.flash('danger')
+  });
 });
 
 // helper function
 function validateEmail (req, res, next) {
   if (email.indexOf(req.body.email) === -1) {
     //throw error
-    req.flash('danger', 'Invalid email. Please try again.')
+    req.flash('danger', req.body.email + ' is an invalid email. Please try again.')
     res.redirect('/')
   } else {
+    console.log('Test');
+    req.flash('danger', 'Success!')
     return next();
   }
   // console.log('more boobs', req.body);
